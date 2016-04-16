c63433bc5cba5e0c4b025beb08064aded1e2888d
Agosits <825629701@qq.com>
2016-04-16 19:31:02 +0800

    Added files via upload

--- views.py
+++ views.py
@@ -6,20 +6,22 @@ from wechat_sdk import WechatBasic
 import json
 import urllib
 
-TOKEN = 'tokenforweixin'
+TOKEN = 'secret'
 APP_ID = 'wxe2916810a9a30626'
-APP_SECRET = 'e38a7c606db6310b20d9345cee57479c'
+APP_SECRET = 'secret'
+
 
 def get_rsp_data(url):
     json_data = urllib.request.urlopen(url).read().decode('utf-8')
     rsp_data = json.loads(json_data)
     return rsp_data
 
+
 @csrf_exempt
 def entrance(request):
     def get_access_token():
         url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+APP_ID+'&secret='+APP_SECRET
-        access_token =  get_rsp_data(url)['access_token']
+        access_token = get_rsp_data(url)['access_token']
         return access_token
 
     def get_user_info():
@@ -27,7 +29,7 @@ def entrance(request):
         url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='+access_token+'&openid='+message.source
         ueser_info_dic = get_rsp_data(url)
         user_info = ""
-        for k,v in ueser_info_dic.items():
+        for k, v in ueser_info_dic.items():
             user_info = user_info+str(k)+":"+str(v)+"\n"
         return user_info
 
@@ -51,20 +53,20 @@ def entrance(request):
         rsp = "failed"
     return HttpResponse(rsp)
 
+
 @csrf_exempt
 def oauth(request):
-    code = request.GET.get('code','error')
-    nsukey = request.GET.get('nsukey','null')
+    code = request.GET.get('code', 'error')
     if code != 'error':
         url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+APP_ID+'&secret='+APP_SECRET+'&code='+code+'&grant_type=authorization_code'
         data = get_rsp_data(url)
         access_token_oauth = data['access_token']
         openid_oauth = data['openid']
 
-        #user_info = 'access_token_oauth:'+access_token_oauth+'\n'+'openid_oauth:'+openid_oauth+'\n' 
         user_info = ''
         url = 'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token_oauth+'&openid='+openid_oauth
         data = get_rsp_data(url)
-        for k,v in data.items():
+        for k, v in data.items():
             user_info = user_info+str(k)+":"+str(v)+"<br>"
         return HttpResponse(user_info)
+        
